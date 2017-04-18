939ab2a2d8db0c4550a338dc576b0cc14ae7569e
Dan Peterson <Dan.Peterson@software.dell.com>
2017-04-18 16:28:12 -0600

    Demo project

--- Cfa533Rs232Demo/Program.cs
+++ Cfa533Rs232Demo/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Cfa533Rs232Demo
+﻿using System;
+using CommandLine;
+using Petrsnd.Cfa533Rs232Driver;
+
+namespace Petrsnd.Cfa533Rs232Demo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            try
+            {
+                using (var device = new LcdDevice("COM3", LcdBaudRate.Baud19200))
+                {
+                    device.Connect();/*
+                    return Parser.Default
+                        .ParseArguments
+                        <>(
+                                args).MapResult(
+                                    ,
+                                    errs => 1);*/
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
         }
     }
 }
